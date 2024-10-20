e245b2ce57d75a292bd687101b49ff92ff4b6b12
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2024-10-19 20:34:36 -0600

    Update Tarea4_ejercicio3.java

--- Tarea 4/Tarea4_ejercicio3.java
+++ Tarea 4/Tarea4_ejercicio3.java
@@ -5,25 +5,21 @@ public class Tarea4_ejercicio3 {
         Scanner scanner = new Scanner(System.in);
         int filas;
 
-        // Solicitar al usuario el número de filas
         System.out.print("Ingrese el número de filas: ");
         filas = scanner.nextInt();
 
-        // Validar que el número de filas sea impar y mayor que 3
         if (filas % 2 == 0 || filas < 3) {
             System.out.println("No se pudo mostrar el programa");
             scanner.close();
             return;
         }
 
-        int mitad = filas / 2; // Calcular la mitad
+        int mitad = filas / 2;
 
-        // Ciclo principal para controlar las filas
         for (int i = 0; i < filas; i++) {
             int numEstrellas;
             int numEspacios;
 
-            // Condiciones para determinar el número de estrellas y espacios
             if (i <= mitad) {
                 numEstrellas = 2 * i + 1;
             } else {
@@ -31,16 +27,14 @@ public class Tarea4_ejercicio3 {
             }
             numEspacios = (filas - numEstrellas) / 2;
 
-            // Ciclo anidado para imprimir los espacios y las estrellas
             for (int j = 0; j < filas; j++) {
                 if (j < numEspacios) {
-                    System.out.print(" "); // Imprimir espacios
+                    System.out.print(" ");
                 } else if (j < numEspacios + numEstrellas) {
-                    System.out.print("*"); // Imprimir estrellas
+                    System.out.print("*");
                 }
             }
 
-            // Salto de línea después de cada fila
             System.out.println();
         }
 
