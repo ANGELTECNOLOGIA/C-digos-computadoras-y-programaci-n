a394a1b9506e7ad24ad564925d98839aaef0a69f
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2024-10-19 20:35:15 -0600

    Update Tarea4_ejercicio2.java

--- Tarea 4/Tarea4_ejercicio2.java
+++ Tarea 4/Tarea4_ejercicio2.java
@@ -12,44 +12,49 @@ public class Tarea4_ejercicio2 {
         lector.close();
 
         for (int i = 1; i <= altura; i++) {
-            for (int j = 1; j <= i; j++)
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
+            }
 
-            for (int k = i + 1; k <= altura; k++)
+            for (int k = i + 1; k <= altura; k++) {
                 System.out.print(" ");
-
+            }
             System.out.print(" ");
             System.out.print(" ");
             System.out.print(" ");
             System.out.print(" ");
 
-            for (int l = 1; l <= altura + 1 - i; l++)
+            for (int l = 1; l <= altura + 1 - i; l++) {
                 System.out.print("*");
-
-            for (int m = altura - 1 - i; m <= altura; m++)
+            }
+            for (int m = altura - 1 - i; m <= altura; m++) {
                 System.out.print(" ");
-
+            }
             System.out.print(" ");
             System.out.print(" ");
             System.out.print(" ");
             System.out.print(" ");
 
-            for (int n = 1; n < i; n++)
+            for (int n = 1; n < i; n++) {
                 System.out.print(" ");
+            }
 
-            for (int p = i; p <= altura; p++)
+            for (int p = i; p <= altura; p++) {
                 System.out.print("*");
+            }
 
             System.out.print(" ");
             System.out.print(" ");
             System.out.print(" ");
             System.out.print(" ");
 
-            for (int q = 1; q <= altura - i; q++)
+            for (int q = 1; q <= altura - i; q++) {
                 System.out.print(" ");
+            }
 
-            for (int r = altura - i; r < altura; r++)
+            for (int r = altura - i; r < altura; r++) {
                 System.out.print("*");
+            }
 
             System.out.println();
         }
