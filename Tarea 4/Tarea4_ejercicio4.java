a93bfa93218bcc0b05d5a30f87ae7e536ef3759c
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2024-10-19 20:33:04 -0600

    Update Tarea4_ejercicio4.java

--- Tarea 4/Tarea4_ejercicio4.java
+++ Tarea 4/Tarea4_ejercicio4.java
@@ -2,29 +2,22 @@ import javax.swing.JOptionPane;
 
 public class Tarea4_ejercicio4 {
     public static void main(String[] args) {
-        // Leer los datos de entrada
-        int n = Integer.parseInt(JOptionPane.showInputDialog("Longitud de la barra:"));
-        int a = Integer.parseInt(JOptionPane.showInputDialog("Longitud del lado vertical:"));
-        int b = Integer.parseInt(JOptionPane.showInputDialog("Longitud del lado horizontal:"));
         
-        // Calcular la longitud de madera necesaria para una puerta
-        int maderaPorPuerta = 2 * a + b;
+        int longitudDeLaBarra = Integer.parseInt(JOptionPane.showInputDialog("Longitud de la barra:"));
+        int ladoVertical = Integer.parseInt(JOptionPane.showInputDialog("Longitud del lado vertical:"));
+        int ladoHorizontal = Integer.parseInt(JOptionPane.showInputDialog("Longitud del lado horizontal:"));
         
-        // Calcular la madera necesaria para dos puertas
+        int maderaPorPuerta = 2 * ladoVertical + ladoHorizontal;
+        
         int maderaNecesaria = 2 * maderaPorPuerta;
         
-        // Inicializar el contador de barras
         int barrasNecesarias = 0;
         
-        // Mientras haya madera por cubrir
         while (maderaNecesaria > 0) {
-            // Restamos la longitud de una barra al total de madera necesaria
-            maderaNecesaria -= n;
-            // Aumentamos el contador de barras necesarias
+            maderaNecesaria -= longitudDeLaBarra;
             barrasNecesarias++;
         }
-        
-        // Mostrar el resultado
+
         JOptionPane.showMessageDialog(null, "El número mínimo de barras necesarias es: " + barrasNecesarias);
     }
 }
